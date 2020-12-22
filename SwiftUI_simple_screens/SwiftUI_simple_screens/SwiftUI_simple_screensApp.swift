f1d9ca383734c27eff304aad469e4fffddf60003
Denis Suprun <denis.suprun@forti.io>
2020-12-22 20:40:24 +0300

    Initial commit

---
+++ SwiftUI_simple_screens/SwiftUI_simple_screens/SwiftUI_simple_screensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_simple_screensApp.swift
+//  SwiftUI_simple_screens
+//
+//  Created by Denis Suprun on 22.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_simple_screensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xib_simple_screens/Xib_simple_screens/Xib_simple_screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xib_simple_screens
+//
+//  Created by Denis Suprun on 22.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
